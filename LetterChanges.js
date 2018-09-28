da4d76e6008f9a58772f5f45b55991f3812f1f94
Alex Kim <alexkim@macs-MacBook-Pro-2.local>
2018-09-28 09:25:31 -0500

    did letter changes

--- LetterChanges.js
+++ LetterChanges.js
@@ -1,9 +1,53 @@
 function LetterChanges(str) {
-  var one = ['a','b','c','d','e','f','g','h','j','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
-  var two = ['b','c','d','e','f','g','h','j','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','a'];
+    // let one = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+    // let two = ["b","c","d","E","f","g","h","I","j","k","l","m","n","O","p","q","r","s","t","U","v","w","x","y","z","A"];
+    var reference = {
+      "a": "b",
+      "b": "c",
+      "c": "d",
+      "d": "E",
+      "e": "f",
+      "f": "g",
+      "g": "h",
+      "h": "I",
+      "i": "j",
+      "j": "k",
+      "k": "l",
+      "l": "m",
+      "m": "n",
+      "n": "O",
+      "o": "p",
+      "p": "q",
+      "q": "r",
+      "r": "s",
+      "s": "t",
+      "t": "u",
+      "u": "v",
+      "v": "w",
+      "w": "x",
+      "x": "y",
+      "y": "z",
+      "z": "A"
+    }
+
+    let emptyArr = [];
+    str = str.toLowerCase();
+    str = str.split("");
+
+    for (var i = 0; i < str.length; i++) {
+      if (reference[str[i]] !== null){
+        emptyArr.push(reference[str[i]]);
+      } else {
+        emptyArr.push(str[i]);
+      }
+    }
 
 
+    return emptyArr.join("");
   // code goes here
-  return str;
+//   return str;
 
 }
+
+// keep this function call here
+LetterChanges(readline());
